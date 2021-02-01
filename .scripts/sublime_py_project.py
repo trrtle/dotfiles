809ac3df87feea4ea79cce72ba85bee2bbeed619
mick <mick@uwkm.nl>
2021-02-01 20:59:59 +0100

    more stuff

--- .scripts/sublime_py_project.py
+++ .scripts/sublime_py_project.py
@@ -5,9 +5,15 @@ import json
 import os
 
 HOME = os.path.expanduser("~")
-DEFAULT_VIRTUALENV_LOCATION = f"{HOME}/devel/venv/virtualenvs/"
-DEFAULT_PROJECT_DIR = f"{HOME}/devel/projects/sublime/"
-CWD = os.getcwd()
+DEFAULT_SUBLIME_PROJECT_DIR = f"{HOME}/devel/projects/sublime/"
+DEFAULT_PROJECT_DIR = f"{HOME}/devel/projects/highbiza/"
+
+try:
+    VIRTUAL_ENV = os.environ["VIRTUAL_ENV"]
+except KeyError:
+    raise Exception("You need a virtual environment to run this script")
+
+PROJECT_NAME = os.path.basename(VIRTUAL_ENV)
 
 
 def arguments():
@@ -18,29 +24,29 @@ def arguments():
         "-p",
         "--project",
         nargs="?",
-        default=CWD,
-        help="Directory where your python projects reside"
+        default=f"{DEFAULT_PROJECT_DIR}{PROJECT_NAME}",
+        help="Directory where the project resides"
+    )
+    parser.add_argument(
+        "-v",
+        "--virtual_env",
+        nargs="?",
+        default=VIRTUAL_ENV,
+        help="Virtual environment location"
     )
     parser.add_argument(
         "-d",
         "--destination",
         nargs="?",
-        default=DEFAULT_PROJECT_DIR,
+        default=DEFAULT_SUBLIME_PROJECT_DIR,
         help="Sublime project file directory"
-    )
-    parser.add_argument(
-        "-v",
-        "--venv_dir",
-        nargs="?",
-        default=DEFAULT_VIRTUALENV_LOCATION,
-        help="Directory where python virtual environments reside"
     )
 
     return parser.parse_args()
 
 
 def create_project_file(args):
-    project_name = os.path.basename(args.project)
+    project_name = os.path.basename(args.virtual_env)
     project = {
         "folders":
         [
@@ -50,7 +56,7 @@ def create_project_file(args):
         ],
         "settings":
         {
-            "python_interpreter": f"{args.venv_dir}{project_name}/bin/python"
+            "python_interpreter": f"{args.virtual_env}/bin/python"
         }
     }
 
@@ -65,9 +71,9 @@ def create_project_file(args):
 
 
 if __name__ == "__main__":
-    args = arguments()
+    args = arguments()    
     file_created = create_project_file(args)
-    project_name = os.path.basename(args.project)
+    project_name = os.path.basename(args.virtual_env)
     project_file = f"{args.destination}{project_name}.sublime-project"
     if file_created:
         print(f"Creating new sublime project file at: {project_file}")
